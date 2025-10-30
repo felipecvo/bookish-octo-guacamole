69a6c40ebf2826002ff6a51a5e449866cc9fbceb
Prof. Felipe Vilella <proffelipe.oliveira@fiap.com.br>
2025-10-30 08:39:46 -0300

    ci: deploy to aci

--- setup.sh
+++ setup.sh
@@ -5,4 +5,4 @@ ACR_NAME=pf2128sprint42tdsa
 
 az group create -n $GROUP -l eastus2
 
-az acr create --resource-group $GROUP --name $ACR_NAME --sku Basic
+az acr create --resource-group $GROUP --name $ACR_NAME --sku Basic --admin-enabled
